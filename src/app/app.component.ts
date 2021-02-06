cd0da35f5829619a47b6d98ef69bbecc542a2daa
Michael <michael_meas@hotmail.com>
2021-02-06 09:19:50 -0300

    Project example error dataview primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,174 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'teste-data-grid';
+export class AppComponent implements OnInit {
+
+  products!: Product[];
+
+  sortOptions!: SelectItem[];
+
+  sortOrder!: number;
+
+  sortField!: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.products =
+      [
+        {
+          "id": "1000",
+          "code": "f230fh0g3",
+          "name": "Bamboo Watch",
+          "description": "Product Description",
+          "image": "bamboo-watch.jpg",
+          "price": 65,
+          "category": "Accessories",
+          "quantity": 24,
+          "inventoryStatus": "INSTOCK",
+          "rating": 5
+        },
+        {
+          "id": "1001",
+          "code": "nvklal433",
+          "name": "Black Watch",
+          "description": "Product Description",
+          "image": "black-watch.jpg",
+          "price": 72,
+          "category": "Accessories",
+          "quantity": 61,
+          "inventoryStatus": "INSTOCK",
+          "rating": 4
+        },
+        {
+          "id": "1002",
+          "code": "zz21cz3c1",
+          "name": "Blue Band",
+          "description": "Product Description",
+          "image": "blue-band.jpg",
+          "price": 79,
+          "category": "Fitness",
+          "quantity": 2,
+          "inventoryStatus": "LOWSTOCK",
+          "rating": 3
+        },
+
+      ]
+      ;
+
+    this.sortOptions = [
+      { label: 'Price High to Low', value: '!price' },
+      { label: 'Price Low to High', value: 'price' }
+    ];
+  }
+
+  onSortChange(event: { value: any; }) {
+    let value = event.value;
+
+    if (value.indexOf('!') === 0) {
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    }
+    else {
+      this.sortOrder = 1;
+      this.sortField = value;
+    }
+  }
+
+  add() {
+    this.products.push({
+      "id": "1003",
+      "code": "244wgerg2",
+      "name": "Blue T-Shirt",
+      "description": "Product Description",
+      "image": "blue-t-shirt.jpg",
+      "price": 29,
+      "category": "Clothing",
+      "quantity": 25,
+      "inventoryStatus": "INSTOCK",
+      "rating": 5
+    },
+      {
+        "id": "1004",
+        "code": "h456wer53",
+        "name": "Bracelet",
+        "description": "Product Description",
+        "image": "bracelet.jpg",
+        "price": 15,
+        "category": "Accessories",
+        "quantity": 73,
+        "inventoryStatus": "INSTOCK",
+        "rating": 4
+      },
+      {
+        "id": "1005",
+        "code": "av2231fwg",
+        "name": "Brown Purse",
+        "description": "Product Description",
+        "image": "brown-purse.jpg",
+        "price": 120,
+        "category": "Accessories",
+        "quantity": 0,
+        "inventoryStatus": "OUTOFSTOCK",
+        "rating": 4
+      },
+      {
+        "id": "1006",
+        "code": "bib36pfvm",
+        "name": "Chakra Bracelet",
+        "description": "Product Description",
+        "image": "chakra-bracelet.jpg",
+        "price": 32,
+        "category": "Accessories",
+        "quantity": 5,
+        "inventoryStatus": "LOWSTOCK",
+        "rating": 3
+      },
+      {
+        "id": "1007",
+        "code": "mbvjkgip5",
+        "name": "Galaxy Earrings",
+        "description": "Product Description",
+        "image": "galaxy-earrings.jpg",
+        "price": 34,
+        "category": "Accessories",
+        "quantity": 23,
+        "inventoryStatus": "INSTOCK",
+        "rating": 5
+      },
+      {
+        "id": "1008",
+        "code": "vbb124btr",
+        "name": "Game Controller",
+        "description": "Product Description",
+        "image": "game-controller.jpg",
+        "price": 99,
+        "category": "Electronics",
+        "quantity": 2,
+        "inventoryStatus": "LOWSTOCK",
+        "rating": 4
+      },
+      {
+        "id": "1009",
+        "code": "cm230f032",
+        "name": "Gaming Set",
+        "description": "Product Description",
+        "image": "gaming-set.jpg",
+        "price": 299,
+        "category": "Electronics",
+        "quantity": 63,
+        "inventoryStatus": "INSTOCK",
+        "rating": 3
+      });
+  }
+
+  remove() {
+    this.products = new Array<Product>();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DataViewModule } from 'primeng/dataview';
+import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { RatingModule } from 'primeng/rating';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { TabViewModule } from 'primeng/tabview';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    DataViewModule,
+    PanelModule,
+    DropdownModule,
+    TabViewModule,
+    InputTextModule,
+    RatingModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
